e2223d3144c6a31b567672e0420833461ae1c639
MKMX <maxo5@yonyou.com>
2019-08-05 17:10:14 +0800

    lodash

--- miao/lodash/mklm0222-lodash.js
+++ miao/lodash/mklm0222-lodash.js
@@ -73,13 +73,17 @@ var mklm0222={
    * param array (Array): 需要处理的数组
    * param [size=1] (number): 每个数组区块的长度
    */
-  chunk:function(arr,size){
-    var result=[]
-    for(var i=0;i<arr.length;i++){
-       while(size>=0&&i<arr.length){
-         result.push
-       }
+  chunk:function(arr,size,result=[]){
+    if(arr.length==0){
+      return result
     }
+    if(arr.length<size){
+      result.push(arr.slice())
+      return result
+    }
+    result.push(arr.slice(0,size))
+    this.chunk(arr.slice(size),size,result)
+    return result
   },
   /**
    * 创建一个具有唯一array值的数组，每个值不包含在其他给定的数组中。（即创建一个新数组，这个数组中的值，为第一个数字（array 参数）排除了给定数组中的值。）该方法使用 SameValueZero做相等比较。结果值的顺序是由第一个数组中的顺序确定。
