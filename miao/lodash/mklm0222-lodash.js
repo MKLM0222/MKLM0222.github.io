55a1b7eb7960f38d74651b34dd1030d8f10e2e12
MKMX <maxo5@yonyou.com>
2019-08-05 17:02:23 +0800

    lodash

--- miao/lodash/mklm0222-lodash.js
+++ miao/lodash/mklm0222-lodash.js
@@ -67,5 +67,58 @@ var mklm0222={
      return function(ary){
        return f.apply(null,ary)
      }
+  },
+  /**
+   * 将数组（array）拆分成多个 size 长度的区块，并将这些区块组成一个新数组。 如果array 无法被分割成全部等长的区块，那么最后剩余的元素将组成一个区块。
+   * param array (Array): 需要处理的数组
+   * param [size=1] (number): 每个数组区块的长度
+   */
+  chunk:function(arr,size){
+    var result=[]
+    for(var i=0;i<arr.length;i++){
+       while(size>=0&&i<arr.length){
+         result.push
+       }
+    }
+  },
+  /**
+   * 创建一个具有唯一array值的数组，每个值不包含在其他给定的数组中。（即创建一个新数组，这个数组中的值，为第一个数字（array 参数）排除了给定数组中的值。）该方法使用 SameValueZero做相等比较。结果值的顺序是由第一个数组中的顺序确定。
+   * array (Array): 要检查的数组。
+   * [values] (...Array): 排除的值。
+   * (Array): 返回一个过滤值后的新数组。
+   */
+  difference:function(ary,values){
+    var res=[]
+    for(var item of ary){
+      if(values.indexOf(item)<0){
+        res.push(item)
+      }
+    }
+    return res
+},
+  differenceBy:function(){
+
+  },
+  drop:function(){
+
+  },
+  dropRight:function(){
+
+  },
+  dropRightWhile:function(){
+
+  },
+  dropWhile:function(){
+
+  },
+  fill:function(){
+
+  },
+  findIndex:function(){
+
+  },
+  findLastIndex:function(){
+
   }
+
 };
